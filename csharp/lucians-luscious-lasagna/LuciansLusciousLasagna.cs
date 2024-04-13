90a40972dbc79cecae6f4e0b14e6ee8f3f47f374
Martin Walsh <1520570+martinwalsh@users.noreply.github.com>
2024-04-13 14:31:18 -0500

    update lucians-luscious-lasagna based on feedback

--- csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -1,24 +1,17 @@
 class Lasagna
 {
-    // TODO: define the 'ExpectedMinutesInOven()' method
-    public int ExpectedMinutesInOven()
-    {
-        return 40;
-    }
+    public int ExpectedMinutesInOven() => 40;
 
-    // TODO: define the 'RemainingMinutesInOven()' method
     public int RemainingMinutesInOven(int minutes)
     {
         return ExpectedMinutesInOven() - minutes;
     }
 
-    // TODO: define the 'PreparationTimeInMinutes()' method
     public int PreparationTimeInMinutes(int layers)
     {
         return layers * 2;
     }
 
-    // TODO: define the 'ElapsedTimeInMinutes()' method
     public int ElapsedTimeInMinutes(int layers, int minutes)
     {
         return PreparationTimeInMinutes(layers) + minutes;
