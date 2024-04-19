a6db6ddfd9723f5d6ee344f4faea7f620c0d188b
Martin Walsh <1520570+martinwalsh@users.noreply.github.com>
2024-04-19 06:05:57 -0500

    attempt nix gnu-cobol

--- shell.nix
+++ shell.nix
@@ -7,7 +7,16 @@ pkgs.mkShell {
     dotnet-sdk_8
     bats
     cmake
-  ];
 
-  inputsFrom = [ ];
+    # # Build gnu-cobol for darwin
+    # (gnu-cobol.overrideAttrs (
+    #   oldAttrs: {
+    #     buildInputs = oldAttrs.buildInputs ++ [ libxml2 ];
+    #     preConfigure = ''
+    #       export XML_CFLAGS="-I${pkgs.libxml2.dev}/include/libxml2"
+    #       export XML_LIBS="-L${pkgs.libxml2.out}/lib -lxml2"
+    #     '';
+    #   }
+    # ))
+  ];
 }
